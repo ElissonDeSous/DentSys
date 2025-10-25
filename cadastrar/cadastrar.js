4d0a4ec73f249a8c42f723b95e4ec70a007de18b
Elisson De Sous <elissondsousa@gmail.com>
2025-10-25 15:00:48 -0300

    adicionando pagina cadastro

---
+++ cadastrar/cadastrar.js
@@ -0,0 +1,34 @@
+const Nome = document.getElementById("nome");
+const Email = document.getElementById("email");
+const Senha = document.getElementById("password");
+
+
+const form = document.getElementById("form");
+const menssagem = document.getElementById("menssagem");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const nome = String(Nome.value);
+  const email = String(Email.value);
+  const senha = String(Senha.value);
+
+  const response = await fetch("https://dentsys-backend.onrender.com/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ nome, email, senha }),
+  });
+  console.log(response)
+
+  const usuario = await response.json();
+
+  if (response.ok) {
+    menssagem.innerHTML = usuario.mensagem;
+    menssagem.style.color = 'green'
+  }
+    else {
+    menssagem.innerHTML = usuario.mensagem;
+    menssagem.style.color = 'red'
+   
+  }
+});
